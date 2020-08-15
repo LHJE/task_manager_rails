088acd7aa53cbee2e02a46525463d6d33a60c279
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-14 19:21:50 -0600

    Still not able to get it working after Finding Specific Tasks

--- task_manager/config/routes.rb
+++ task_manager/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
-  # get '/tasks/:id', to: "tasks#show"
+  get '/tasks/:id', to: "tasks#show"
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
